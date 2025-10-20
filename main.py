0ee16c1017dad3dccc861715a4376d1a6b662606
SuPuHe <mizin.a.s.2006@gmail.com>
2025-10-21 00:37:56 +0200

    create start/stop system and make it turnable

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-def on_received_number(receivedNumber):
-    if True:
-        pass
-radio.on_received_number(on_received_number)
+def StartMotors():
+    pins.digital_write_pin(DigitalPin.P12, 1)
+    pins.digital_write_pin(DigitalPin.P13, 1)
+
+def StopMotors():
+    pins.digital_write_pin(DigitalPin.P12, 0)
+    pins.digital_write_pin(DigitalPin.P13, 0)
 
 def on_received_value(name, value):
-    pass
+    if name == "sigleft":
+        if value == 1:
+            pins.servo_write_pin(AnalogPin.P3, 45)
+            basic.show_icon(IconNames.SMALL_HEART)
+    elif name == "sigright":
+        if value == 1:
+            pins.servo_write_pin(AnalogPin.P3, 90)
+            basic.show_icon(IconNames.HEART)
+    elif name == "sigup":
+        if value == 1:
+            basic.show_icon(IconNames.YES)
+    elif name == "sigdown":
+        if value == 1:
+            basic.show_icon(IconNames.TSHIRT)
 radio.on_received_value(on_received_value)
 
 radio.set_group(228)
+pins.digital_write_pin(DigitalPin.P14, 1)
+pins.analog_write_pin(AnalogPin.P0, 1023)
 
 def on_forever():
     pass
--- main.ts
+++ main.ts
@@ -1,10 +1,20 @@
+function StopMotors () {
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+}
+function StartMotors () {
+    pins.digitalWritePin(DigitalPin.P12, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+}
 radio.onReceivedValue(function (name, value) {
     if (name == "sigleft") {
         if (value == 1) {
+            pins.servoWritePin(AnalogPin.P3, 45)
             basic.showIcon(IconNames.SmallHeart)
         }
     } else if (name == "sigright") {
         if (value == 1) {
+            pins.servoWritePin(AnalogPin.P3, 90)
             basic.showIcon(IconNames.Heart)
         }
     } else if (name == "sigup") {
@@ -15,9 +25,15 @@ radio.onReceivedValue(function (name, value) {
         if (value == 1) {
             basic.showIcon(IconNames.TShirt)
         }
+    } else if (name == "sigstart") {
+        StartMotors()
+    } else if (name == "sigstop") {
+        StopMotors()
     }
 })
 radio.setGroup(228)
+pins.digitalWritePin(DigitalPin.P14, 1)
+pins.analogWritePin(AnalogPin.P0, 1023)
 basic.forever(function () {
 	
 })
